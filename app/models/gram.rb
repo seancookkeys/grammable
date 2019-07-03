eda6b272e1616cb485e4b764173a31f72d890ddd
Sean Cook <you@sean.bernard.cook@gmail.com>
2019-07-03 01:37:57 +0000

    finished lesson 18

--- app/models/gram.rb
+++ app/models/gram.rb
@@ -2,5 +2,6 @@ class Gram < ApplicationRecord
     mount_uploader :picture, PictureUploader
     validates :message, presence: true
     validates :picture, presence: true
+    has_many :comments
     belongs_to :user
 end
